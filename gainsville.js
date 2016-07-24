3e59afbbeb1fe7178604f6a39e791840b714c0f5
Jason Bao <jabao@ucsd.edu>
2016-07-24 15:50:58 -0700

    Add files via upload

--- gainsville.js
+++ gainsville.js
@@ -23,15 +23,12 @@
   function createTable() {
     var body = document.body,
         tbl  = document.createElement('table');
-    tbl.style.width  = '100px';
-    tbl.style.border = '1px solid black';
 
     for(var i = 0; i < 3; i++){
         var tr = tbl.insertRow();
         for(var j = 0; j < 3; j++){
           var td = tr.insertCell();
           td.appendChild(document.createTextNode('Cell'));
-          td.style.border = '1px solid black';
         }
     }
     body.appendChild(tbl);
