ca481322d773ac9204dc6d62ab49238f242b0378
Jason Bao <jabao@ucsd.edu>
2016-08-07 03:57:44 -0700

    actually fixed wording

--- gainsville.js
+++ gainsville.js
@@ -1,5 +1,4 @@
-
-  var goal, days, parts; // global variables that affect workout
+ var goal, days, parts; // global variables that affect workout
 
   window.onload = function() { // pull information from query string
     var query = window.location.search;
@@ -13,20 +12,18 @@
       return 0;
     }
 
-    var data = query.split(',');
-    var temp = data.splice(0, 2);
-    goal = temp[0];
-    days = temp[1];
-    parts = data;
+    parts = query.split(',');
+    goal = "strength";
+    createTable(arrayFill());
   }
 
   function arrayFill() { // populate 2D array
     var tempArr = [];
     var counter = 0;
-    while(counter < parts) {
+    while(counter < parts.length) {
       switch(parts[counter]) { // populate array based on part
         case "deltoid":
-          if(goal == "strength") {
+          if(goal === "strength") {
             tempArr[counter] = ["Deltoid", "Overhead Press", "3 x 5"];
           }
 
@@ -35,7 +32,7 @@
           }
           break;
         case "bicep":
-          if(goal == "strength") {
+          if(goal === "strength") {
             tempArr[counter] = ["Bicep", "Barbell Bicep Curl", "3 x 8"];
           }
 
@@ -47,7 +44,7 @@
           tempArr[counter] = ["Forearm", "Dumbbell Wrist Curl", "3 x 12"];
           break;
         case "quadricep":
-          if(goal == "hypertrophy") {
+          if(goal === "hypertrophy") {
             tempArr[counter] = ["Quadricep", "Leg Extension", "3 x 12"];
           }
 
@@ -56,7 +53,7 @@
           }
           break;
         case "chest":
-          if(goal == "hypertrophy") {
+          if(goal === "hypertrophy") {
             tempArr[counter] = ["Chest", "Cable Fly", "3 x 12"];
           }
 
@@ -65,7 +62,7 @@
           }
           break;
         case "tricep":
-          if(goal == "strength") {
+          if(goal === "strength") {
             tempArr[counter] = ["Tricep", "Close Grip Bench Press", "3 x 8"];
           }
 
@@ -77,7 +74,7 @@
           tempArr[counter] = ["Abdomen", "Plank", "3 x As long as possible"];
           break;
         case "hamstring":
-          if(goal == "strength") {
+          if(goal === "strength") {
             tempArr[counter] = ["Hamstring", "Leg Curl", "3 x 8"];
           }
 
@@ -89,7 +86,7 @@
           tempArr[counter] = ["Calves", "Standing Calf Raise", "3 x 15"];
           break;
         case "trapezius":
-          if(goal == "hypertrophy") {
+          if(goal === "hypertrophy") {
             tempArr[counter] = ["Trapezius", "Barbell Shrug", "3 x 12"];
           }
 
@@ -102,7 +99,7 @@
           }
           break;
         case "upper":
-          if(goal == "hypertrophy") {
+          if(goal === "hypertrophy") {
             tempArr[counter] = ["Upper Back", "Cable Row", "3 x 12"];
           }
 
@@ -111,7 +108,7 @@
           }
           break;
         case "lower":
-          if(goal == "hypertrophy") {
+          if(goal === "hypertrophy") {
             tempArr[counter] = ["Lower Back", "Hyperextension", "3 x 12"];
           }
 
